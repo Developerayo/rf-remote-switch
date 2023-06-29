00150638cbfceb4cf1a2f3f2d35a90788f08c624
Shodipo Ayomide <shodipovi@gmail.com>
2023-06-29 23:20:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(12, GPIO.OUT) # Channel 1
 GPIO.setup(16, GPIO.OUT) # Channel 2
 
-GPIO.setup(18, GPIO.IN)
+GPIO.setup(18, GPIO.IN) 
 
 def signal_received(pin):
 
